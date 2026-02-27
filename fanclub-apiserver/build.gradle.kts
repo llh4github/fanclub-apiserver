6a3bcba3250926cf5eff1c0dfb6d04979e099872
llh4github <lilinhong_coding@foxmail.com>
2026-02-27 17:07:38 +0800

    chore: 将子模块中重复的配置项提升到根目录build.gradle.kts文件中

---
+++ fanclub-apiserver/build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    kotlin("jvm")
+    kotlin("plugin.spring")
+    id("org.springframework.boot")
+    id("io.spring.dependency-management")
+    id("org.graalvm.buildtools.native")
+    alias(libs.plugins.gradle.git.properties)
+    alias(libs.plugins.ksp)
+}
+
+description = "fanclub-apiserver"
+
+dependencies {
+    implementation(libs.oshai)
+    implementation(libs.yitter.idgenerator)
+    implementation("org.springframework.boot:spring-boot-starter-webmvc")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("tools.jackson.module:jackson-module-kotlin")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-websocket")
+    testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
