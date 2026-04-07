15704a80a211475b113570a8c13c8d19688631ea
llh4github <lilinhong_coding@foxmail.com>
2026-04-07 17:30:50 +0800

    feat(viewer): 添加BV点播次数统计功能
    - 在ViewerscBvRecord dto中添加ViewScBvCountSpec规格定义
    - 在ViewerscBvRecordService接口中添加bvCount方法用于统计BV点播次数
    - 在ViewerscBvRecordServiceImpl实现类中实现bvCount方法并添加缓存注解
    - 导入相关依赖包包括ViewScBvCountSpec、id扩展函数和缓存辅助类
    - 添加count查询相关的Kotlin SQL客户端导入
    - 注释掉当前的缓存查询逻辑并使用TODO标记待完善

--- fanclub-apiserver/src/main/kotlin/service/viewer/ViewerscBvRecordService.kt
+++ fanclub-apiserver/src/main/kotlin/service/viewer/ViewerscBvRecordService.kt
@@ -1,8 +1,14 @@
 package llh.fanclubvup.apiserver.service.viewer
 
 import llh.fanclubvup.apiserver.entity.viewer.ViewerScBvRecord
+import llh.fanclubvup.apiserver.entity.viewer.dto.ViewScBvCountSpec
 import llh.fanclubvup.apiserver.service.BaseDatabaseService
 
 interface ViewerScBvRecordService : BaseDatabaseService<ViewerScBvRecord> {
+
+    /**
+     * bv点播过几次了
+     */
+    fun bvCount(spec: ViewScBvCountSpec): Long
 
 }
--- fanclub-apiserver/src/main/kotlin/service/viewer/impl/ViewerscBvRecordServiceImpl.kt
+++ fanclub-apiserver/src/main/kotlin/service/viewer/impl/ViewerscBvRecordServiceImpl.kt
@@ -1,8 +1,10 @@
 package llh.fanclubvup.apiserver.service.viewer.impl
 
 import llh.fanclubvup.apiserver.entity.viewer.ViewerScBvRecord
+import llh.fanclubvup.apiserver.entity.viewer.dto.ViewScBvCountSpec
 import llh.fanclubvup.apiserver.service.BaseDatabaseServiceImpl
 import llh.fanclubvup.apiserver.service.viewer.ViewerScBvRecordService
+import llh.fanclubvup.ksp.annon.CacheNameGen
 import org.babyfish.jimmer.sql.kt.KSqlClient
 import org.springframework.stereotype.Service
 
@@ -11,5 +13,17 @@ class ViewerScBvRecordServiceImpl(
     sqlClient: KSqlClient,
 ) : ViewerScBvRecordService,
     BaseDatabaseServiceImpl<ViewerScBvRecord>(ViewerScBvRecord::class, sqlClient) {
-
+    @CacheNameGen
+    override fun bvCount(spec: ViewScBvCountSpec): Long {
+//        return cacheData(
+//            ViewerScBvRecordServiceCacheHelper.BvCountCachePrefix,
+//            ViewerScBvRecordServiceCacheHelper.BvCountCachePrefix,
+//            ){
+//            createQuery {
+//                where(spec)
+//                select(count(table.id))
+//            }.fetchOne()
+//        }?:0L
+        TODO()
+    }
 }
