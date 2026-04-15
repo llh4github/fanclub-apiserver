2d91f3fe1d60c42708a3b2ebfc296c218f97e36c
llh4github <lilinhong_coding@foxmail.com>
2026-04-15 14:41:27 +0800

    feat(utils): 添加主播身份判断和anchorId获取功能
    - 引入BID类型支持
    - 新增isAnchor()方法用于判断当前用户是否为主播
    - 新增anchorId()方法用于获取当前用户anchorId
    - 添加相应的KDoc注释说明功能用途

--- fanclub-apiserver/src/main/kotlin/utils/SecurityContextUtil.kt
+++ fanclub-apiserver/src/main/kotlin/utils/SecurityContextUtil.kt
@@ -6,8 +6,9 @@
 package llh.fanclubvup.apiserver.utils
 
 import io.github.oshai.kotlinlogging.KotlinLogging
-import llh.fanclubvup.apiserver.dto.security.JwtUserLoginAuthenticationToken
 import llh.fanclubvup.apiserver.consts.enums.RoleEnums
+import llh.fanclubvup.apiserver.dto.security.JwtUserLoginAuthenticationToken
+import llh.fanclubvup.common.BID
 import llh.fanclubvup.common.excptions.AppRuntimeException
 import org.springframework.security.core.context.SecurityContextHolder
 
@@ -18,10 +19,23 @@ object SecurityContextUtil {
         return authenticationToken().userId
     }
 
+    /**
+     * 判断当前用户是否是主播
+     */
+    fun isAnchor(): Boolean {
+        return RoleEnums.ANCHOR == authenticationToken().roleType
+    }
+
     fun currentRole(): RoleEnums {
         return authenticationToken().roleType
     }
 
+    /**
+     * 获取当前用户anchorId
+     */
+    fun anchorId(): BID? {
+        return authenticationToken().anchorId
+    }
 
     fun currentUsername(): String {
         return authenticationToken().uname
