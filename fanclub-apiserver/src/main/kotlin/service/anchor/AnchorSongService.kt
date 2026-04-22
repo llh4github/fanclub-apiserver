4c55660f0d697774fa218e95d39a732da938e942
llh4github <lilinhong_coding@foxmail.com>
2026-04-23 00:07:35 +0800

    feat: 为AnchorSong添加默认唯一键列表

--- fanclub-apiserver/src/main/kotlin/service/anchor/AnchorSongService.kt
+++ fanclub-apiserver/src/main/kotlin/service/anchor/AnchorSongService.kt
@@ -9,8 +9,15 @@ import llh.fanclubvup.apiserver.dto.DeleteIds
 import llh.fanclubvup.apiserver.entity.anchor.AnchorSong
 import llh.fanclubvup.apiserver.service.BaseDatabaseService
 import llh.fanclubvup.common.BID
+import kotlin.reflect.KProperty1
 
 interface AnchorSongService : BaseDatabaseService<AnchorSong> {
+
+    companion object {
+
+        val defaultUniqueKeys: List<KProperty1<AnchorSong, *>> =
+            listOf(AnchorSong::bid, AnchorSong::name)
+    }
 
     fun deleteByIds(ids: List<Long>, bid: BID?): Int
 
